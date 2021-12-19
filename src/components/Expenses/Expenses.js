9ee65f64e8ca1bfaed81d821d2486ab7d2209fe8
Antonios Zachos <55564849+antoniozah@users.noreply.github.com>
2021-12-20 00:22:45 +0200

    fixing expenses element style

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,13 +2,13 @@ import Expense from '../Expense/Expense';
 import './Expenses.css';
 
 const Expenses = ({ expenseData }) => {
-  return (
-    <div className='expenses'>
-      {expenseData.map((expense) => (
-        <Expense expense={expense} key={expense.id} />
-      ))}
-    </div>
-  );
+    return (
+        <div className="expenses box-style">
+            {expenseData.map((expense) => (
+                <Expense expense={expense} key={expense.id} />
+            ))}
+        </div>
+    );
 };
 
 export default Expenses;
