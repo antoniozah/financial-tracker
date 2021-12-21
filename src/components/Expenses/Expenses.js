73bc0fef92b66e8fc94ccf568544bdd0c7ee407f
Antonios Zachos <55564849+antoniozah@users.noreply.github.com>
2021-12-21 22:35:16 +0200

    adding filter by year component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import Expense from '../Expense/Expense';
+import ExpenseFilter from '../ExpenseFilter/ExpenseFilter';
 import './Expenses.css';
 
 const Expenses = ({ expenseData }) => {
+    const [yearFilter, setYearFilter] = useState('2020');
+
+    const filterChangeYear = (year) => {
+        setYearFilter(year);
+    };
+
     return (
         <div className="expenses box-style">
+            <ExpenseFilter
+                onYearChange={filterChangeYear}
+                selectedYear={yearFilter}
+            />
             {expenseData.map((expense) => (
                 <Expense expense={expense} key={expense.id} />
             ))}
